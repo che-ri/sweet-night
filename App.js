dfd962c909201e943ee176b295b8ead70ba6cbeb
che-ri <rnjs1135@gmail.com>
2022-01-09 23:09:44 +0900

    chore: 환경세팅

---
+++ App.js
@@ -0,0 +1,14 @@
+//components
+import Header from "./shared/Header";
+import Cafe from "./pages/Cafe";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Cafe />
+    </div>
+  );
+}
+
+export default App;
